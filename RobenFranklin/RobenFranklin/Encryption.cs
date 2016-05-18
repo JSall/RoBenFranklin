bfa5362432ab24652ccd0d2f9b5b960850e804c5
JSall <jas11v@my.fsu.edu>
2016-05-18 15:03:00 +0100

    Added RSA Encrpytion, and encrpyted the twitter keys

---
+++ RobenFranklin/RobenFranklin/Encryption.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobenFranklin
+{
+    public class Encryption
+    {
+
+        private RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(2048);
+
+        //Create a new instance of the RSAParameters structure.
+
+        public Encryption()
+        {
+            string privateKey, publicKey;
+
+            using (StreamReader reader = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "TextFiles\\Privatekey.xml"))
+            {
+                privateKey = reader.ReadToEnd();
+            }
+            using (StreamReader reader = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "TextFiles\\Publickey.xml"))
+            {
+                publicKey = reader.ReadToEnd();
+            }
+
+            RSA.FromXmlString(publicKey);
+
+            RSA.FromXmlString(privateKey);
+
+        }
+
+        public string Encrypt(string plainText)
+        {
+
+
+            byte[] encryptedBytes = RSA.Encrypt(UTF8Encoding.UTF8.GetBytes(plainText), false);
+
+            return Convert.ToBase64String(encryptedBytes);
+
+        }
+
+
+        public string Decrypt(string text)
+        {
+
+
+
+            byte[] textAsBytes = Convert.FromBase64String(text);
+
+            byte[] decryptedBytes = RSA.Decrypt(textAsBytes, false);
+
+            return UTF8Encoding.UTF8.GetString(decryptedBytes);
+        }
+    }
+}
--- RobenFranklin/RobenFranklin/Program.cs
+++ RobenFranklin/RobenFranklin/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using TweetSharp;
@@ -13,27 +14,30 @@ namespace RobenFranklin
     {
         static void Main(string[] args)
         {
+
             UpdateRoBenFranklin();
+
 
         }
 
         private static void UpdateRoBenFranklin()
         {
-            AppSettingsReader settingsReader =new AppSettingsReader();
+            AppSettingsReader settingsReader = new AppSettingsReader();
+            Encryption enc = new Encryption();
 
             MarkovChain chain = new MarkovChain(10);
             FeedText(chain);
             List<string> tweets = new List<string>();
 
-            var twitterApp = new TwitterService((string)settingsReader.GetValue("ConsumerKey",
-                                                     typeof(String)),
-                                                (string)settingsReader.GetValue("ConsumerSecret",
-                                                     typeof(String)));
+            var twitterApp = new TwitterService(enc.Decrypt((string)settingsReader.GetValue("ConsumerKey",
+                                                     typeof(String))),
+                                                enc.Decrypt((string)settingsReader.GetValue("ConsumerSecret",
+                                                     typeof(String))));
 
-            twitterApp.AuthenticateWith((string)settingsReader.GetValue("TokenKey",
-                                                     typeof(String)),
-                                                (string)settingsReader.GetValue("TokenSecret",
-                                                     typeof(String)));
+            twitterApp.AuthenticateWith(enc.Decrypt((string)settingsReader.GetValue("TokenKey",
+                                                     typeof(String))),
+                                               enc.Decrypt((string)settingsReader.GetValue("TokenSecret",
+                                                     typeof(String))));
 
             for (int i = 1; i <= 15; i++)
             {
@@ -73,5 +77,43 @@ namespace RobenFranklin
             }
 
         }
+
+        //    public static void GenerateRsa(string privateKeyPath, string publicKeyPath, int size)
+        //    {
+        //        //stream to save the keys
+        //        FileStream fs = null;
+        //        StreamWriter sw = null;
+
+        //        //create RSA provider
+        //        RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(size);
+        //        try
+        //        {
+        //            //save private key
+        //            fs = new FileStream(privateKeyPath, FileMode.Create, FileAccess.Write);
+        //            sw = new StreamWriter(fs);
+        //            sw.Write(rsa.ToXmlString(true));
+        //            sw.Flush();
+        //        }
+        //        finally
+        //        {
+        //            if (sw != null) sw.Close();
+        //            if (fs != null) fs.Close();
+        //        }
+
+        //        try
+        //        {
+        //            //save public key
+        //            fs = new FileStream(publicKeyPath, FileMode.Create, FileAccess.Write);
+        //            sw = new StreamWriter(fs);
+        //            sw.Write(rsa.ToXmlString(false));
+        //            sw.Flush();
+        //        }
+        //        finally
+        //        {
+        //            if (sw != null) sw.Close();
+        //            if (fs != null) fs.Close();
+        //        }
+        //        rsa.Clear();
+        //    }
     }
 }
